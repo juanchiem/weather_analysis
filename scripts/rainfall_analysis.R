c040b19b334d4cc1228770aec888206730cdcfd3
juanchiem <edwardsmolina@gmail.com>
2019-05-09 14:16:21 -0300

    minor changes

--- scripts/rainfall_analysis.R
+++ scripts/rainfall_analysis.R
@@ -1,13 +1,18 @@
 pacman::p_load(tidyverse, lubridate)
 source(here::here('scripts/theme_weather.R'))
 
+# Cálculos resumen y visualización gráfica de las precipitaciones acumuladas por década (10+1/-2 dias)
+#-----------------------------------------------------------------------------------
+
+# Importar datos y agregar algunas columnas de fechas
+
 bce <- readxl::read_excel(here::here("data/balcarce_clima.xlsx")) %>%
   mutate(year = lubridate::year(date),
-         month = lubridate::month(date),
-         julian = lubridate::yday(date))
+         month = lubridate::month(date))
 
-# Calcular lluvias acumuladas por decada
-bce_serie <-
+# Calcular lluvias acumuladas por década
+
+bce_serie <- # Para la serie histórica
   bce %>%
   filter(year!=2018) %>%
   group_by(date=if_else(day(date) >= 30,
@@ -17,7 +22,6 @@ bce_serie <-
             days = n()) %>%
   mutate(year = year(date),
          month = month(date)) %>%
-  # julian = yday(date)) %>%
   ungroup %>%
   group_by(year) %>%
   mutate(decada = row_number()) %>%
@@ -28,7 +32,7 @@ bce_serie <-
             lower_80=quantile(rain_acum, .2, na.rm = T), # Rango 80% de los años
             upper_80=quantile(rain_acum, .8, na.rm = T))
 
-bce_18_19 <-
+bce_18_19 <- # Para la reciente campaña
   bce %>%
   group_by(date = if_else(day(date) >= 30,
                           floor_date(date, "20 days"),
@@ -40,34 +44,36 @@ bce_18_19 <-
   mutate(decada = row_number()) %>%
   filter(date > '2018-08-31', date < '2019-03-30')
 
-bce_18_19 <-
+bce_18_19 <- # fusionar ambos datasets (serie + campaña)
   bce_18_19 %>%
   left_join(bce_serie, by = c("decada")) %>%
   mutate(date = as.Date(date))
 
-# print(bce_18_19, n=Inf)
-# print(bce_serie, n=Inf)
+# print(bce_18_19, n=Inf) # Check
+# print(bce_serie, n=Inf) # Check
 
-p_5=position_nudge(x = 5)
+# Visualizar la serie y sus bandas de 80-percentil, y valores acumulados por década de la última campaña
+
 (p1 <-
     bce_18_19 %>%
     ggplot(aes(x=date)) +
     geom_pointrange(aes(y=med, ymin=lower_80, ymax=upper_80), fill='white', color='deepskyblue',
-                    shape=21, fatten=.7, size=3, position=p_5)+
+                    shape=21, fatten=.7, size=3, position=(p5=position_nudge(x = 5)))+
     geom_point(aes(y=rain_acum_season), col ="brown1",
-               position=p_5) +
-    geom_line(aes(y=rain_acum_season, group=1), col ="brown1", linetype="dashed", position=p_5)+
+               position=p5) +
+    geom_line(aes(y=rain_acum_season, group=1), col ="brown1", linetype="dashed", position=p5)+
     scale_y_continuous(limits=c(0, 100), expand=c(0.05, 0))+
     scale_x_date(date_breaks="1 month", date_labels="%b", expand=expand_scale(0.05,0))
 )
 
+# Fenología del girasol
 r5 = c(as.Date("2018-12-15"),  as.Date("2019-01-15")); diff(r5)
 r6 = c(as.Date("2019-01-15"),  as.Date("2019-03-10")); diff(r6)
 midpoint <- function(interval) { min(interval)+(max(interval)-min(interval))/2 }; midpoint(r5)
 
-(p_final <-
+(p_final <- # Agregar fenología y anotaciones varias
     p1 +
-    # barras de floracion y llenado
+    # Polígonos de floracion y llenado
     annotate("rect", xmin= c(min(r5), min(r6)), xmax=c(max(r5),max(r6)), ymin=70, ymax=90, alpha=0.3, fill=c("grey80","grey60"))+
     annotate("text", x = c(midpoint(r5), midpoint(r6)), y=80,col="red", parse=TRUE, size=3,
              label=c('bold("Floración")','bold("Llenado")')) +
@@ -77,5 +83,4 @@ midpoint <- function(interval) { min(interval)+(max(interval)-min(interval))/2 }
          # "mm acumulados por períodos de 10 días",
          caption = "Datos registrados en la estación meteorológica de la EEA INTA Balcarce")
 )
-
 # ggsave(file = "plots/bce_lluvias.png", w=80, h=50, units="mm", dpi=300, scale=2)
--- scripts/temperature_analysis.R
+++ scripts/temperature_analysis.R
@@ -1,13 +1,19 @@
 pacman::p_load(tidyverse, lubridate)
 source(here::here('scripts/theme_weather.R'))
 
+# Cálculos resumen y visualización gráfica de las temperaturas medias diarias para la serie historica y reciente campaña de grano grueso
+#-----------------------------------------------------------------------------------
+
+# Importar datos y agregar algunas columnas de fechas
+
 bce <- readxl::read_excel(here::here("data/balcarce_clima.xlsx")) %>%
   mutate(year = lubridate::year(date),
          month = lubridate::month(date),
          julian = lubridate::yday(date))
 
-# construir la banda central de Tmin y Tmax de cada dia
-bce_serie <- bce %>%
+# Calcular 80-percentil de Tmean diarias y valores maximos y minimos observados en la serie histórica
+bce_serie <-
+  bce %>%
   filter(year<2018) %>%
   group_by(julian) %>%
   summarise(
@@ -24,12 +30,14 @@ bce_serie <- bce %>%
 # save(bce, bce_serie, file = "~/Dropbox/Proyectos/clima bce/bce_serie.RData")
 # load("~/Dropbox/Proyectos/clima bce/bce_serie.RData")
 
-bce_18_19 <-
+bce_18_19 <- # Fusionar serie con campaña 18/19
   bce %>%
   filter(date > '2018-08-31', date < '2019-03-30') %>%
   left_join(bce_serie, by = c("julian", "month")) %>%
   mutate(date = as.Date(date)) %>%
   droplevels()
+
+# Visualizar la Tmean en la serie (con sus bandas de 80-percentil), y valores  por década de la última campaña
 
 (p1 <- bce_18_19 %>%
   ggplot(aes(x=date)) +
@@ -44,6 +52,7 @@ rec_frio <- bce_18_19[which(bce_18_19$tmean<bce_18_19$lower_tmean),]
 rec_cal <- bce_18_19[which(bce_18_19$tmean>bce_18_19$upper_tmean),]
 
 library(ggrepel); options(repr.plot.width = 2, repr.plot.height = 2)
+
 (p2 <- p1 +
   # Agregar medias record frío
   geom_point(data = rec_frio, aes(date, y = tmean), colour = "blue") +
@@ -61,18 +70,20 @@ library(ggrepel); options(repr.plot.width = 2, repr.plot.height = 2)
   annotate("text", x=c(as.Date("2018-09-3"), as.Date("2018-11-10")), y=c(0,3.5),
            label=c("Helada meteorológica","Heladas\nagronómicas"), size=3, hjust=0))
 
+# Fenología del girasol
 r5 = c(as.Date("2018-12-15"),  as.Date("2019-01-15")); diff(r5)
 r6 = c(as.Date("2019-01-15"),  as.Date("2019-03-10")); diff(r6)
 midpoint <- function(interval) { min(interval)+(max(interval)-min(interval))/2 }; midpoint(r5)
 
-(p_final <- p2 +
-    # barras de floracion y llenado
+(p_final <- # Agregar fenología y anotaciones varias
+    p2 +
+    # barras de floración y llenado
     annotate("rect", xmin= c(min(r5), min(r6)), xmax=c(max(r5),max(r6)), ymin=0, ymax=30, alpha=0.2, fill=c("grey80","grey60"))+
     annotate("text", x = c(midpoint(r5), midpoint(r6)), y=3,col="red", parse=TRUE, size=3,
              label=c('bold("Floración")','bold("Llenado")')) +
     labs(x=NULL, y=NULL,
-         title = "Temperatura media en Balcarce y estadíos reproductivos del girasol",
+         title = "Temperatura media diaria en Balcarce y estadíos reproductivos del girasol",
          subtitle = "- Campaña 2018/19 (línea negra)\n- Serie 1971-2017: rango 80% (banda interna) y medias extremas (bandas externas)",
          caption = "Datos registrados en la estación meteorológica de la EEA INTA Balcarce"))
-p_final
+
 # ggsave(file = "plots/bce_temp.png", w=80, h=50, units="mm", dpi=300, scale=2)
